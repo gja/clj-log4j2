d4a6790aa52fe19f0ccfd04f923c7728c9fe514b
Tejas Dinkar <tejas@gja.in>
2021-12-13 09:33:15 +0530

    Updating log4j for CVE-2021-44228

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject clj-log4j2 "0.2.0"
+(defproject clj-log4j2 "0.3.0"
   :description "Easily add log4j2 into your clojure project"
   :url "http://github.com/gja/clj-log4j2"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.apache.logging.log4j/log4j-core "2.3"]
-                 [org.apache.logging.log4j/log4j-api "2.3"]])
+                 [org.apache.logging.log4j/log4j-core "2.15.0"]
+                 [org.apache.logging.log4j/log4j-api "2.15.0"]])
